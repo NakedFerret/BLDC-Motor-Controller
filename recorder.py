133ec26b91f93d86173c19f716f6d9063c5919b3
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-10-31 22:01:00 -0700

    First attempt at using pyserial

---
+++ recorder.py
@@ -0,0 +1,22 @@
+import serial
+import argparse
+
+parser = argparse.ArgumentParser(description='Record voltage readings.')
+
+parser.add_argument('port', help='which port to listen to')
+parser.add_argument('baud', type=int, help='baud rate of the port')
+
+args = parser.parse_args()
+
+print 'Listening to %s with baud rate: %d' % (args.port, args.baud)
+port = serial.Serial(args.port)
+
+print 'Waiting for input'
+
+while(True):
+  text = port.readline();
+  print text
+
+print 'Ready to run..'
+
+port.close()     
