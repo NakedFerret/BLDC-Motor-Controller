5301efa20daed376dc507cfb2e290941c86c904d
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-11-01 19:47:41 -0700

    Recorder will wait for input, send signal, read readings, and print them

--- recorder.py
+++ recorder.py
@@ -14,9 +14,26 @@ port = serial.Serial(args.port, baudrate=args.baud)
 print 'Waiting for input'
 
 while(True):
-  text = port.readline();
-  print text
+  text = port.readline()
+  if text.strip() == 'A':
+    break
 
-print 'Ready to run..'
+print 'Sending start signal'
+
+port.write('A');
+
+
+readings = []
+while(True):
+  text = port.readline().strip()
+  if text != 'A':
+    readings.append(text)
+  else:
+    break
+
+print 'Got these readings'
+
+for r in readings:
+  print r
 
 port.close()     
