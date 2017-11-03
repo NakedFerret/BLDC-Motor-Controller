686e8f5be7ea8b03062a2b4817186af08b4587a3
Gonzalo Andreani <gonzaandreani@gmail.com>
2017-11-02 22:01:50 -0700

    Speed up the analogReads!

--- arduino-sketch/arduino-sketch.ino
+++ arduino-sketch/arduino-sketch.ino
@@ -3,8 +3,22 @@ unsigned long currentTime;
 int sensor0Value;
 int count;
 
+
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
 // the setup routine runs once when you press reset:
 void setup() {
+  // set prescale to 16
+  sbi(ADCSRA,ADPS2) ;
+  cbi(ADCSRA,ADPS1) ;
+  cbi(ADCSRA,ADPS0) ;
+  
   Serial.begin(250000);
   startTime = 0;
   waitForSignal();
